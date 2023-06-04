4f8c6b05a3d7c53a336f2331d615205baaa3a815
farachot <valkanid12@gmail.com>
2023-06-04 21:18:02 +0700

    edit admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\Admin\HomeController::class, 'index'])->name('homepage');
+
+Route::get('admin/dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard.index');
